7d3cc011b166b8a21171c3e7d5268b3551ddd44a
chundla <57375541+chundla@users.noreply.github.com>
2026-05-20 11:23:02 -0400

    Initial Commit

---
+++ nexusTests/nexusTests.swift
@@ -0,0 +1,18 @@
+//
+//  nexusTests.swift
+//  nexusTests
+//
+//  Created by Chandler on 5/18/26.
+//
+
+import Testing
+
+struct nexusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
